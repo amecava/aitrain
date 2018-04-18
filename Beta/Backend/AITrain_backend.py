e24d4e2e3d9b811e750c511140f9bb29ba0019f0
Amedeo Cavallo <amedeo.cavallo96@gmail.com>
2018-04-18 14:39:32 +0200

    Backend initialization

--- Beta/Backend/AITrain_backend.py
+++ Beta/Backend/AITrain_backend.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-No name project
+AITrain backend
 '''
 # -*- encoding: utf-8 -*-
 
@@ -8,10 +8,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+import data_parser
+
 import tensorflow as tf
 
 def main(argv):
-    print(argv)
+    data_parser.main()
 
 if __name__ == '__main__':
     tf.app.run()
