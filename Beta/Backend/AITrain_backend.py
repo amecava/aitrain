9d1d564410da46ab146a6f9d7cac841d1ce80f82
Amedeo Cavallo <amedeo.cavallo96@gmail.com>
2018-04-18 13:22:09 +0200

    Reorganization

--- Beta/Backend/AITrain_backend.py
+++ Beta/Backend/AITrain_backend.py
@@ -1,5 +1,17 @@
-def main (argv):
-    #####
+#!/usr/bin/env python3
+'''
+No name project
+'''
+# -*- encoding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import tensorflow as tf
+
+def main(argv):
+    print(argv)
 
 if __name__ == '__main__':
     tf.app.run()
